86f392dd724486ebfde421bc0f7bb165c722651d
Ranger-Jay <careerforjay@gmail.com>
2023-05-16 22:41:12 +0000

    upload random card code, needs a refresh button to make new card, will do one day

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable - init commit test origin */
+/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +7,48 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+
+  let newNumber = document.querySelector(".number");
+
+  let newSuitA = document.querySelector(".suitA");
+  let newSuitB = document.querySelector(".suitB");
+
+  function randomCard(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  function card() {
+    let number = numbers[randomCard(numbers)];
+    let suit = suits[randomCard(suits)];
+
+    newNumber.innerHTML = number;
+    newSuitA.innerHTML = suit;
+    newSuitB.innerHTML = suit;
+
+    if (suit == "&hearts;" || suit == "&diams;") {
+      newSuitA.style.color = "red";
+      newSuitB.style.color = "red";
+      newNumber.style.color = "red";
+    }
+  }
+
+  //
+  card();
 };
